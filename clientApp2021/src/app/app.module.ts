b9123336e187ee00bd623e533655df4d3eae92ec
S00197531 <S00197531@VLAB-STDNT-10>
2021-12-13 17:24:18 +0000

    parent child done

--- clientApp2021/src/app/app.module.ts
+++ clientApp2021/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { BookListComponent } from './book2/book-list/book-list.component';
+import { BookRowComponent } from './book2/book-row/book-row.component';
+import { BookDetailsComponent } from './book2/book-details/book-details.component';
+import { SampleFormComponent } from './book2/sample-form/sample-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    BookListComponent,
+    BookRowComponent,
+    BookDetailsComponent,
+    SampleFormComponent
   ],
   imports: [
     BrowserModule,
